06c2d79714fc689fef0cf871cf2fa926b3e0da2f
Mehmet Ali Peker <maps6134@hotmail.com>
2017-09-17 16:46:34 +0300

    Adden js file

---
+++ auto-page-reloader.js
@@ -0,0 +1,16 @@
+reload = true;
+
+function autoPageReloader(time) {
+    time = (typeof time !== 'undefined') ?  time : 3000;
+    reloadTimeOut = setTimeout(function () {
+        location.reload();
+    }, time);
+    window.onfocus = function () {
+        clearTimeout(reloadTimeOut);
+    };
+    window.onblur = function() {
+        reloadTimeOut = setTimeout(function () {
+            location.reload();
+        }, time);
+    };
+}
